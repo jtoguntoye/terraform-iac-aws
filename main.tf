c8b120d59aecf264a984d01f8720baea22cf0887
Joel Oguntoye <tosinjoel3@gmail.com>
2021-10-20 12:41:21 +0100

    Add gitignore file

--- main.tf
+++ main.tf
@@ -1,28 +1,53 @@
-provider "aws" {
-    region = "eu-west-3"
-}
+# create region variable
+    variable "region" {
+        default =  "eu-west-3"
+    }
+    variable "vpc_cidr" {
+        default = "172.16.0.0/16"
+    }
+    variable "enable_dns_support" {
+        default = "true"
+    }
+
+    variable "enable_dns_hostnames" {
+        default ="true" 
+    }
+
+    variable "enable_classiclink" {
+        default = "false"
+    }
+
+    variable "enable_classiclink_dns_support" {
+        default = "false"
+    }
+
+    variable "preferred_number_of_public_subnets" {
+        default = "2"
+    }
+
+    provider "aws" {
+        region = var.region
+    }
 
 # create VPC
-resource "aws_vpc" "main" {
-    cidr_block = "172.16.0.0/16"
-    enable_dns_support = "true"
-    enable_dns_hostnames = "true"
-    enable_classiclink = "false"
-    enable_classiclink_dns_support = "false" 
-}
+    resource "aws_vpc" "main" {
+        cidr_block = var.vpc_cidr
+        enable_dns_support = var.enable_dns_support 
+        enable_dns_hostnames = var.enable_dns_hostnames
+        enable_classiclink = var.enable_classiclink
+        enable_classiclink_dns_support = var.enable_classiclink_dns_support
+    }
+
+#Get list of available zones using data sources
+    data "aws_availability_zones" "available" {
+        state = "available"
+    }    
 
 # create public subnet1
-resource "aws_subnet" "public1" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = "172.16.0.0/24"
-    map_public_ip_on_launch = true
-    availability_zone = "eu-west-3a"
-}
-
-# create public subnet2
-resource "aws_subnet" "public2" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = "172.16.1.0/24"
-    map_public_ip_on_launch = true
-    availability_zone = "eu-west-3b"
-}
+    resource "aws_subnet" "public" {
+        count = var.preferred_number_of_public_subnets == null ? length(data.aws_availability_zones.available.names) :var.preferred_number_of_public_subnets
+        vpc_id = aws_vpc.main.id
+        cidr_block = cidrsubnet(var.vpc_cidr, 4, count.index)
+        map_public_ip_on_launch = true
+        availability_zone = data.aws_availability_zones.available.names[count.index]
+    }
