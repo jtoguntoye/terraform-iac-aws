17b2ff14ac55ba02d8582f2a247763474336e641
Joel Oguntoye <tosinjoel3@gmail.com>
2021-11-03 16:26:52 +0100

    create s3 bucket and dynamoDB table for state remote backend and state locking

---
+++ backend.tf
@@ -0,0 +1,27 @@
+resource "aws_s3_bucket" "terraform_State" {
+    bucket = "joekiff-dev-terraform-bucket"
+    acl = "private"
+    
+    # enable versioning 
+    versioning {
+      enabled = true
+    }
+    # enable server side encryption  by default
+    server_side_encryption_configuration {
+    rule {
+        apply_server_side_encryption_by_default {
+            sse_algorithm = "AES256"
+        }
+    }
+    }
+}
+
+resource "aws_dynamodb_table" "terraform-locks" {
+  name = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
+  attribute {
+      name = "LockID"
+      type = "S"
+  }
+}
